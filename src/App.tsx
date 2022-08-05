5bdcf6a7e259c67b2c7b2fd4c1111951a4002211
DolanL <dlakshinov@inbox.ru>
2022-08-05 17:37:26 +0300

    Added Dialogs component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './App.css'
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
-  return (
-    <div className="app__wrapper">
-        <Header/>
-        <Navbar/>
-        <Profile/>
-    </div>
-  );
+    return (
+        <div className="app__wrapper">
+            <Header/>
+            <Navbar/>
+            <div className="app__content__wrapper">
+                <Dialogs/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
